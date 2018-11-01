2d14ba3deae3fd0ac5dcc1cee27b44c0d5619331
Cicero8419 <44686154+Cicero8419@users.noreply.github.com>
2018-11-01 20:09:23 -0300

    Create teste.sh

---
+++ teste.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#Meu Primeiro Script
+
+#listando os diretorios do linux
+ls -lha /
+read
+clear
+
+#Atualizando o apt ou apt-get
+apt update
+echo "Listas atualizadas!!!"
+read
+cls
+
+#Desligando o servidor
+shutdown -h now
